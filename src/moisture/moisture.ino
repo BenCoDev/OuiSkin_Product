e6f4f6a252a9f0a9b1ab0d8900e0e42eb0845799
Benoit Cotte <benoit.cotte@essec.edu>
2014-05-03 15:42:51 +0200

    Add time in measures and message if measure is 0

--- src/moisture/moisture.ino
+++ src/moisture/moisture.ino
@@ -1,15 +1,37 @@
-int sensorPin = A0;    // select the input pin for the potentiometer
+const int sensorPin = 0;    // select the input pin for the sensor
 int sensorValue = 0;  // variable to store the value coming from the sensor
+long time; //var to store time
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
+  //  setup serial
    Serial.begin(9600);  
 }
 
 void loop() {
+  
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);    
-  delay(1000);          
-  Serial.print("sensor = " );                       
-  Serial.println(sensorValue);                   
+  
+  // If value != 0
+  if (analogRead(sensorPin))  
+  { 
+    // start the timer 
+    time = millis();
+    
+    // measure and print during 3 sec
+    while(millis() - time <= 3000)
+    {
+      sensorValue = analogRead(sensorPin);
+      Serial.print("sensor = " );                       
+      Serial.println(sensorValue);
+      delay(500);
+    }
+    // end of measure
+    Serial.print("Measure recorded\n");    
+  }
+  else 
+  {
+     Serial.print("Place the sensor\n");
+     delay(500);
+  }
 }
